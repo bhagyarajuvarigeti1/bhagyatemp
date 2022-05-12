53353e0b587510e6884e782e51bbd03710286c95
bhagyarajuvarigeti1 <bhagyarajuvarigeti@gmail.com>
2022-05-12 08:31:42 +0530

    [bhagya] first shellscript program

---
+++ shellScript.sh
@@ -0,0 +1,14 @@
+#! /bin/bash -x
+
+echo "welcome  to shell script"
+
+#initialize variable
+read -p "Enter your name " name
+#name="bhagya raju"
+echo $name
+
+
+echo "enter your city"
+read city
+
+echo $city
