d41cf00a516e2685e081c7632c1f87fed02e4619
bhagyarajuvarigeti1 <bhagyarajuvarigeti@gmail.com>
2022-05-12 21:39:27 +0530

    for loop example

---
+++ forloop.sh
@@ -0,0 +1,20 @@
+#! /bin/bash -x
+
+#for loop
+for file in `ls *.txt`
+do 
+	echo $file
+done
+
+for((i=0;i<10;i++))
+do 
+	echo $i
+done
+
+#while loop
+count=0
+while [ $count -le 5 ]
+do
+	echo $count
+	((count++))
+done
